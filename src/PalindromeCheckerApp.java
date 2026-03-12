d0953fbcda1b394f13622c94099816fe8322ba1c
Aadith-Geeth-Mohan <aadithgeethmohan2007@gmail.com>
2026-03-12 10:06:29 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,27 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-
-    private static final String VERSION = "1.0";
-
 
     public static void main(String[] args) {
 
-        displayWelcomeMessage();
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    private static void displayWelcomeMessage() {
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
 
-        System.out.println("==============================================");
-        System.out.println("     Welcome to Palindrome Checker System     ");
-        System.out.println("==============================================");
-        System.out.println("Version : " + VERSION);
-        System.out.println("System initialized successfully.");
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
     }
 }
